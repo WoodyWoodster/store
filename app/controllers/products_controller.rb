39717e80de157e08b2cf13860dec5bbea7525828
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-26 21:18:17 -0600

    added create functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_param)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_param
+      params.expect(product: [ :name ])
+    end
 end
