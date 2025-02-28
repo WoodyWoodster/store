ea979dbbcd76006a800e16c81e1f83e0fd2a18d3
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-02-27 18:29:25 -0600

    added inventory_count

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,6 +44,6 @@ class ProductsController < ApplicationController
     end
 
     def product_param
-      params.expect(product: [ :name, :description, :featured_image ])
+      params.expect(product: [ :name, :description, :featured_image, :inventory_count ])
     end
 end
